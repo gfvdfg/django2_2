7b5d502e1afa777683d0f36cf31891b4ee0c47d4
gfvdfg <phumi.kehanadee@gmail.com>
2024-09-18 16:03:41 +0700

    update

--- myweb/blogs/views.py
+++ myweb/blogs/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import blogs
 
 def blogs(request):
+  mymembers = blogs.objects.all().values()
   template = loader.get_template('blogs.html')
-  return HttpResponse(template.render())
+  context = {
+    'blogs': blogs,
+  }
+  return HttpResponse(template.render(context, request))
